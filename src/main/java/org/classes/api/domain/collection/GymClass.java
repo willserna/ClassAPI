86b97aa1ac4a0144a818ee2a1849b2e7b7cde02c
William Serna <williamsernac@hotmail.com>
2023-03-23 20:32:22 -0500

    Validation setup

--- src/main/java/org/classes/api/domain/collection/GymClass.java
+++ src/main/java/org/classes/api/domain/collection/GymClass.java
@@ -1,9 +1,11 @@
 package org.classes.api.domain.collection;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.classes.api.domain.user.User;
+import org.classes.api.domain.user.UserDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -19,12 +21,16 @@ public class GymClass {
     @Id
     private String id = UUID.randomUUID().toString();
 
+    @NotBlank(message="Class name field must be filled")
+    @NotNull(message ="Class name is required")
     private String className;
 
+    @NotBlank(message="Coach name must be filled")
+    @NotNull(message ="Coach name is required")
     private String coachName;
 
     private String classTime;
 
-    private List<User> users;
+    private List<UserDTO> users;
 
 }
--- src/main/java/org/classes/api/domain/dto/GymClassDTO.java
+++ src/main/java/org/classes/api/domain/dto/GymClassDTO.java
@@ -1,9 +1,11 @@
 package org.classes.api.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.classes.api.domain.user.User;
+import org.classes.api.domain.user.UserDTO;
 
 import java.util.List;
 
@@ -14,11 +16,15 @@ public class GymClassDTO {
 
     private String id;
 
+    @NotBlank(message="Class name field must be filled")
+    @NotNull(message ="Class name is required")
     private String className;
 
+    @NotBlank(message="Coach name must be filled")
+    @NotNull(message ="Coach name is required")
     private String coachName;
 
     private String classTime;
 
-    private List<User> users;
+    private List<UserDTO> users;
 }
